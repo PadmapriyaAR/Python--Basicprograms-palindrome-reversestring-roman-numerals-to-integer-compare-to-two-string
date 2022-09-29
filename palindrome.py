9e5d1df1155d649651efb503d0d3ed6597d9624b
PadmapriyaAR <109935430+PadmapriyaAR@users.noreply.github.com>
2022-09-29 23:18:56 +1000

    Update and rename random.py to palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def palindrome(str):
+    length = len(str)
+    rev_str = ""
+    #reverse the given string.
+    for x in range(length-1, -1, -1):
+         rev_str += str[x]    
+    if str == rev_str:
+        print(str,"is a Palindrome")
+    else:
+        print(str,"ïs not a Palindrome")
+
+if __name__ == "__main__":
+    palindrome(input("Enter a String : "))
